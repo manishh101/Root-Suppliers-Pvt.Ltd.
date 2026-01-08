91085a2ab69def871d5b5858f8abc20cc8cb1bac
manishh101 <manishhrajbanshi@gmail.com>
2026-01-08 17:05:37 +0545

    Refactor: Rename project directory and update codebase

--- root-suppliers-v2/src/components/modals/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ProductInquiryModal } from "./ProductInquiryModal";
--- root-suppliers-v2/src/components/sections/index.ts
+++
@@ -1,9 +0,0 @@
-export { HeroCarousel } from "./HeroCarousel";
-export { FeaturedProducts, FeaturedProductsStatic } from "./FeaturedProducts";
-export { TopBrands, TopBrandsStatic } from "./TopBrands";
-export { CategoriesSection, CategoriesSectionStatic } from "./CategoriesSection";
-export { AboutSection } from "./AboutSection";
-export { StatsSection } from "./StatsSection";
-export { TestimonialsSection, TestimonialsSectionStatic } from "./TestimonialsSection";
-export { CTASection } from "./CTASection";
-export { VisitUsSection } from "./VisitUsSection";
